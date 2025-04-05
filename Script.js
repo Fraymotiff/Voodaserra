c943edc7ed93cf0e266df043423cf47d83f659dc
Fraymotiff <emily.annie413@gmail.com>
2025-04-05 16:37:38 -0300

    styles: formatting code

--- Script.js
+++ Script.js
@@ -3,17 +3,13 @@ const hamMenu = document.querySelector(".ham-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
 const darkOverlay = document.querySelector(".overlay");
 
-
-
-
 // open off screen menu and make menu close upon clicking overlay
 
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
-darkOverlay.classList.toggle("active");
+  darkOverlay.classList.toggle("active");
 });
-
 
 // toggles overlay
 
@@ -22,16 +18,6 @@ darkOverlay.addEventListener("click", () => {
   offScreenMenu.classList.toggle("active");
 });
 
-if (offScreenMenu.contains ("active")) {
-  
+if (offScreenMenu.contains("active")) {
   darkOverlay.classList.remove("active");
-
 }
-
-
-//closes off screen menu upon clicking menu itself
-
-offScreenMenu.addEventListener("click", () => {
-  offScreenMenu.classList.toggle("active");
-
-});
