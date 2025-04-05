d3ce463a5649bef4f4078131f984d5af4e5b84c8
Fraymotiff <emily.annie413@gmail.com>
2025-04-05 07:08:05 -0300

    styles: acessibility on html, trying out an overlay and deleting it from js and css

--- Script.js
+++ Script.js
@@ -1,8 +1,32 @@
 const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
+const darkOverlay = document.querySelector(".overlay");
+
+
+
+
+
 
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+
+darkOverlay.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+
+
+
+
+
+if (offScreenMenu.contains ("active")) {
+  
+  darkOverlay.classList.toggle("active")
+
+}
+
